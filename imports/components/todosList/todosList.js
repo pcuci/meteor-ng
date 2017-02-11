bfb4065140f6006e40a84c7422bb19813362bbd2
Paul Cuciureanu <paulcu@gmail.com>
2017-02-11 11:56:41 -0500

    Filtering by completed and count

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -7,13 +7,31 @@ class TodosListCtrl {
   constructor($scope) {
     $scope.viewModel(this);
 
+    this.hideCompleted = false;
+
     this.helpers({
       tasks() {
-        return Tasks.find({}, {
+        const selector = {};
+
+        // if hide completed is checked, filter tasks
+        if (this.getReactively('hideCompleted')) {
+          selector.checked = {
+            $ne: true
+          };
+        }
+
+        return Tasks.find(selector, {
           sort: {
             createdAt: -1
           }
         });
+      },
+      incompleteCount() {
+        return Tasks.find({
+          checked: {
+            $ne: true
+          }
+        }).count();
       }
     });
   }
