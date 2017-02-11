3b1dfd0f7f92082bf348804681684e94cc56b0b6
Paul Cuciureanu <paulcu@gmail.com>
2017-02-11 12:49:48 -0500

    Using meteor methods instead of directly accessing the database from the UI

--- imports/components/todosList/todosList.js
+++ imports/components/todosList/todosList.js
@@ -43,12 +43,7 @@ class TodosListCtrl {
 
   addTask(newTask) {
     // Insert a task into the Collection
-    Tasks.insert({
-      text: newTask,
-      createdAt: new Date,
-      owner: Meteor.userId(),
-      username: Meteor.user().username
-    });
+    Meteor.call('tasks.insert', newTask);
 
     // Clear form
     this.newTask = '';
@@ -56,15 +51,11 @@ class TodosListCtrl {
 
   setChecked(task) {
     // Set the checked property to the opposite of its current value
-    Tasks.update(task._id, {
-      $set: {
-        checked: !task.checked
-      }
-    });
+    Meteor.call('tasks.setChecked', task._id, !task.checked);
   }
 
   removeTask(task) {
-    Tasks.remove(task._id);
+    Meteor.call('tasks.remove', task._id);
   }
 }
 
